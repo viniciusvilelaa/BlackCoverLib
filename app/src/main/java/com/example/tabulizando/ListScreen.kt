1df133b972f8836a856839ab2c4c73d830783b1c
viniciusvilelaa <ninobez@icloud.com>
2025-01-10 09:22:36 -0300

    Estilizando card de amostragem de livro do ListScreen

--- app/src/main/java/com/example/tabulizando/ListScreen.kt
+++ app/src/main/java/com/example/tabulizando/ListScreen.kt
@@ -1,30 +1,46 @@
 package com.example.tabulizando
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -42,7 +58,8 @@ fun ListScreen(dbHelper: DataBaseHelper, navController: NavController){
         books.value = dbHelper.listAll(dbHelper)
     }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()
+        .padding(WindowInsets.statusBars.asPaddingValues())) {
         LazyColumn(
         ) {
             items(books.value){ book ->
@@ -50,7 +67,11 @@ fun ListScreen(dbHelper: DataBaseHelper, navController: NavController){
             }
         }
 
-        Button(onClick = {backScreen(navController)}, colors = ButtonDefaults.buttonColors(Color.Red)) {
+        Button(onClick = {backScreen(navController)}, colors = ButtonDefaults.buttonColors(Color.Red),
+            modifier = Modifier.fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(15)
+        ) {
             Text(text = "Voltar",
                 fontSize = 15.sp)
         }
@@ -60,26 +81,30 @@ fun ListScreen(dbHelper: DataBaseHelper, navController: NavController){
 }
 
 
-
+@Preview
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun BookItem(book: Book){
     Card(modifier = Modifier.fillMaxWidth()
         .clickable { println("CLICADO") }
-        .padding(16.dp)){
-        Row {
+        .padding(16.dp),
+        shape = RoundedCornerShape(15),
+        elevation = CardDefaults.cardElevation(10.dp)){
+        Row (modifier = Modifier.padding(10.dp)){
             Column {
                 GlideImage(model = book.imgUrl, contentDescription = "Imagem de um livro", modifier = Modifier
-                    .clip(CircleShape)
                     .size(100.dp)
                     .scale(1.0f))
             }
 
-            Row {
+            Row (verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxSize()){
                 Column {
-                    Text(text = book.title)
-                    Text(text = book.publisher)
-                    Text(text = book.isbn.toString())
+                    Text(text = book.title,
+                        style = TextStyle(fontSize = 22.sp))
+                    Text(text = book.publisher,
+                        fontStyle = FontStyle.Italic, fontSize = 18.sp)
+                    Text(text = book.isbn.toString(), fontWeight = FontWeight.Bold)
                 }
 
             }
